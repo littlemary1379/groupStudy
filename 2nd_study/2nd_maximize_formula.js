fcfba9a936bdb35be7388c8fe62b3932a51a9bb3
mary <littlemary1379@gmail.com>
2021-11-07 10:06:53 +0900

    수식최대화 93점

--- 2nd_study/2nd_maximize_formula.js
+++ 2nd_study/2nd_maximize_formula.js
@@ -3,29 +3,42 @@ function maximize(formula) {
     let formulaArray = formula.split(/[+|*|-]/);
     let regex = /[0-9]/;
     const expressionFilter = formula.split(regex).filter((element)=> element !== '');
+    let result = 0;
 
     console.log(expressionFilter);
     console.log(formulaArray)
 
     for (var index in expression) {
         console.log(calculation(formulaArray, expressionFilter, expression[index]));
+        let calResult = Math.abs(calculation(formulaArray, expressionFilter, expression[index]));
+        if(result < calResult) {
+            result = calResult;
+           
+        }
+        
     };
+
+    return result;
 }
 
 function calculation(formulas, expression, primaryExpression) {
         
-        let calFormula = formulas;
-        let calExpression = expression;  
+        let calFormula = [];
+        let calExpression = [];
+        
+        calFormula.push(...formulas)
+        calExpression.push(...expression)
         
         primaryExpression.forEach(sign => {
 
             console.log("????" + sign)
 
-            calExpression.find((value, index) => {
+            //문제점 : index가 하나씩 줄어드니까.. 하나가 사라지면 나머지 하나의 index가 비틀림...
+            calExpression.forEach((value, index) => {
                 let count = 0;
                 if(value == sign) {
                     console.log(sign, " : ", index)
-                    let realIndex = index - count;
+                    let realIndex = index;
                     let cal;
                     switch(value) {
                         case "-" : {
@@ -43,15 +56,29 @@ function calculation(formulas, expression, primaryExpression) {
 
                     }
 
-                    calFormula.splice(realIndex, 2, cal)
-                    calExpression.splice(realIndex, 1)
+                    // calFormula.splice(realIndex, 2, cal)
+                    // calExpression.splice(realIndex, 1)
+                    
+                    delete calExpression[index];
+                    delete calFormula[index];
+                    delete calFormula[index+1];
+                    calFormula[index+1] = cal
+
                     console.log(calFormula);
                     console.log(calExpression);    
 
                     count += 1;
                 }
+
     
-            });        
+            }); 
+            
+            
+            calFormula = calFormula.filter(Boolean);
+            calExpression = calExpression.filter(Boolean);
+
+            console.log(calFormula);
+            console.log(calExpression);    
             
         });
 
@@ -59,4 +86,4 @@ function calculation(formulas, expression, primaryExpression) {
 }
 
 
-maximize("100-200*300+500+20");
+maximize("100-200*300-500+20");
